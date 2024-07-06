ba5f608bc80f7f2bf0ca30bd1151e8fcc3b85d64
hya62 <hansoo_yoon@sfu.ca>
2024-07-05 21:17:47 -0700

    added local storage

--- script.js
+++ script.js
@@ -4,6 +4,59 @@ const amount = document.getElementById("amount");
 const incomeDisplay = document.getElementById("income-list");
 const expenseDisplay = document.getElementById("expense-list");
 const balance = document.getElementById("balance");
+let incomeItems = [];
+let expenseItems = [];
+
+let appendItem = (parent, item) => {
+  let li = document.createElement("li");
+  li.className = "item";
+  let descriptionText = document.createElement("div");
+  descriptionText.className = "itemDescription";
+  descriptionText.innerHTML = item.description;
+  let amountText = document.createElement("div");
+  amountText.className = "itemAmount";
+  amountText.innerHTML = "$" + item.amount;
+  let dateText = document.createElement("div");
+  dateText.className = "itemDate";
+  dateText.innerHTML = item.date;
+  let deleteButton = document.createElement("button");
+  deleteButton.className = "itemDelete";
+  deleteButton.innerHTML = "Delete";
+  deleteButton.addEventListener("click", deleteTransaction);
+  li.appendChild(descriptionText);
+  li.appendChild(amountText);
+  li.appendChild(dateText);
+  li.appendChild(deleteButton);
+  li.setAttribute("type", item.type);
+  li.setAttribute("key", item.key);
+  parent.appendChild(li);
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  incomeItems = localStorage.getItem("income")
+    ? JSON.parse(localStorage.getItem("income"))
+    : [];
+  expenseItems = localStorage.getItem("expense")
+    ? JSON.parse(localStorage.getItem("expense"))
+    : [];
+  incomeItems.forEach((item) => {
+    appendItem(incomeDisplay, item);
+  });
+  expenseItems.forEach((item) => {
+    appendItem(expenseDisplay, item);
+  });
+  document.getElementById("income-total").innerHTML = localStorage.getItem(
+    "incomeTotal"
+  )
+    ? localStorage.getItem("incomeTotal")
+    : "0";
+  document.getElementById("expense-total").innerHTML = localStorage.getItem(
+    "expenseTotal"
+  )
+    ? localStorage.getItem("expenseTotal")
+    : "0";
+  recalculateBalance();
+});
 
 function recalculateBalance() {
   balance.innerHTML =
@@ -30,20 +83,37 @@ function addTransaction() {
     deleteButton.className = "itemDelete";
     deleteButton.innerHTML = "Delete";
     deleteButton.addEventListener("click", deleteTransaction);
+    let key = new Date().toString();
     li.appendChild(descriptionText);
     li.appendChild(amountText);
     li.appendChild(dateText);
     li.appendChild(deleteButton);
     li.setAttribute("type", type.options[type.selectedIndex].value);
+    li.setAttribute("key", key);
+    let itemObject = {
+      description: description.value,
+      amount: parseInt(amount.value),
+      date: dateText.innerHTML,
+      type: type.options[type.selectedIndex].value,
+      key: key,
+    };
     let total;
+    let totalType;
     if (type.options[type.selectedIndex].value === "Income") {
       incomeDisplay.appendChild(li);
+      totalType = "incomeTotal";
       total = document.getElementById("income-total");
+      incomeItems.push(itemObject);
+      localStorage.setItem("income", JSON.stringify(incomeItems));
     } else {
       expenseDisplay.appendChild(li);
+      totalType = "expenseTotal";
       total = document.getElementById("expense-total");
+      expenseItems.push(itemObject);
+      localStorage.setItem("expense", JSON.stringify(expenseItems));
     }
     total.innerHTML = parseInt(total.innerHTML) + parseInt(amount.value);
+    localStorage.setItem(totalType, total.innerHTML);
     recalculateBalance();
     description.value = "";
     amount.value = "";
@@ -54,11 +124,15 @@ function deleteTransaction(e) {
   if (e.target.className === "itemDelete") {
     let totalType = e.target.parentElement.getAttribute("type");
     let total;
+    let displayType;
     if (totalType === "Income") {
-      total = document.getElementById("income-total");
+      displayType = "income-total";
+      totalType = "incomeTotal";
     } else {
-      total = document.getElementById("expense-total");
+      displayType = "expense-total";
+      totalType = "expenseTotal";
     }
+    total = document.getElementById(displayType);
     total.innerHTML =
       parseInt(total.innerHTML) -
       parseInt(
@@ -68,5 +142,20 @@ function deleteTransaction(e) {
       );
     recalculateBalance();
     e.target.parentElement.remove();
+    if (displayType === "income-total") {
+      incomeItems = incomeItems.filter(
+        (item) => item.key !== e.target.parentElement.getAttribute("key")
+      );
+      localStorage.setItem("income", JSON.stringify(incomeItems));
+    } else {
+      expenseItems = expenseItems.filter(
+        (item) => item.key !== e.target.parentElement.getAttribute("key")
+      );
+      localStorage.setItem("expense", JSON.stringify(expenseItems));
+    }
+    localStorage.setItem(
+      totalType,
+      document.getElementById(displayType).innerHTML
+    );
   }
 }
